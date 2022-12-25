148672bb18e1e7f6b7339978988e867968ac07a4
shell <shell@toss.im>
2022-12-25 16:01:17 +0900

    chore: 빌드 구현

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { join } from 'path';
+
+const srcRoot = join(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
