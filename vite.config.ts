7f302857dab0c1f14fc4d230aa85f142d6655b1e
shell <shell@toss.im>
2022-12-25 18:22:30 +0900

    chore: vanilla-extract설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { join } from 'path';
 
 const srcRoot = join(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   server: {
     port: 3000,
     host: true,
